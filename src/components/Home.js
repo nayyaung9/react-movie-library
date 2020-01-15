aee717050d9f7a9d39474cfc83cbab09d6d4d986
nayyaung9 <naygit.developer@gmail.com>
2020-01-15 22:53:29 +0630

    fetch and render genres movie type lists

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,30 +3,61 @@ import { connect } from 'react-redux';
 import { popularActions } from '../actions/popular';
 import { upcomingActions } from '../actions/upcoming';
 import { nowPlayingActions } from '../actions/nowPlaying';
+import { genresActions } from '../actions/genres';
 
+import { Drawer, Button } from 'antd';
 
 class Home extends React.Component {
+  state = { visible: false };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
   componentDidMount() {
     this.props.getAllPopularMovies();
     this.props.getAllUpcomingMovies();
     this.props.getNowPlayingMovies();
+    this.props.getGenresListType();
   }
   render() {
     return (
       <div className="container">
+          <Button type="primary" onClick={this.showDrawer}>
+          Open
+        </Button>
+        <Drawer
+          title="Basic Drawer"
+          placement="left"
+          closable={false}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          {this.props.genres.map((item, index) => {
+            return <p key={index}>{item.name}</p>
+          })}
+        </Drawer>
         <h5>In Theaters</h5>
         <div className="scrollmenu">
           { this.props.nowPlaying.map((item, index) => {
             return (
               <span key={index}>
                 <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${item.poster_path}`} alt={item.title} />
+
                 <span>{item.title}</span>
               </span>
             )
           })}
         </div>
         <h5>Upcoming movies</h5>
-        <div className="row">
+        <div className="row text-center">
           {this.props.popular.map((item, index) => {
             return (
               <div className="col-md-4" key={index}>
@@ -45,7 +76,8 @@ const mapStateToProps = state => {
   return {
     popular: state.popularMovies.popular,
     upcoming: state.upcomingMovies.upcoming,
-    nowPlaying: state.nowPlaying.nowplaying
+    nowPlaying: state.nowPlaying.nowplaying,
+    genres: state.genres.genres
   }
 }
 
@@ -53,7 +85,8 @@ const mapDispatchToProps = dispatch => {
   return {
     getAllPopularMovies: () => dispatch(popularActions.getAllPopularMovies()),
     getAllUpcomingMovies: () => dispatch(upcomingActions.getAllUpComingMovies()),
-    getNowPlayingMovies: () => dispatch(nowPlayingActions.getNowPlayingMovies())
+    getNowPlayingMovies: () => dispatch(nowPlayingActions.getNowPlayingMovies()),
+    getGenresListType: () => dispatch(genresActions.getGenresListType())
   }
 }
 
--- src/reducers.js
+++ src/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import popular from './reducers/popular';
 import upcoming from './reducers/upcoming';
 import nowPlaying from './reducers/nowPlaying';
+import genres from './reducers/genres';
 
 export default combineReducers({
   popularMovies: popular,
   upcomingMovies: upcoming,
-  nowPlaying
+  nowPlaying,
+  genres
 })
